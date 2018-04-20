5300357ee271ee3dd78ec5e5b95ddfa5fcb44db5
owsorber <38570999+owsorber@users.noreply.github.com>
2018-04-20 18:30:58 -0400

    Update OmniHardware.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniHardware.java
@@ -39,37 +39,36 @@ public class OmniHardware {
    public final double OMNI_CIRCUMFERENCE = OMNI_DIAMETER * 3.14; // in inches
 
    public OmniHardware(HardwareMap hardwareMap) {
-       //constructs object based on hardwareMap of opMode
-       backLeftMotor = hardwareMap.get(DcMotor.class, "blm");
-       backRightMotor = hardwareMap.get(DcMotor.class, "brm");
-       middleMotor = hardwareMap.get(DcMotor.class, "mm");
-       leftMec = hardwareMap.get(DcMotor.class, "lm");
-       rightMec = hardwareMap.get(DcMotor.class, "rm");
-       leftPully = hardwareMap.get(DcMotor.class, "lp");
-       rightPully = hardwareMap.get(DcMotor.class, "rp");
-       horizontalSlide = hardwareMap.get(DcMotor.class, "hs");
-       colorSensor = hardwareMap.get(ColorSensor.class, "cs");
-       gyroSensor = hardwareMap.get(GyroSensor.class, "gs");
-       middleMotor = hardwareMap.get(DcMotor.class, "mm");
-       colorServo = hardwareMap.get(Servo.class, "colorservo");
-       //lights = hardwareMap.get(DcMotor.class, "lights");
-       relicGrabber = hardwareMap.get(Servo.class, "relicgrabber");
-       relicWrist = hardwareMap.get(Servo.class, "relicwrist");
-
+      //constructs object based on hardwareMap of opMode
+      backLeftMotor = hardwareMap.get(DcMotor.class, "blm");
+      backRightMotor = hardwareMap.get(DcMotor.class, "brm");
+      middleMotor = hardwareMap.get(DcMotor.class, "mm");
+      leftMec = hardwareMap.get(DcMotor.class, "lm");
+      rightMec = hardwareMap.get(DcMotor.class, "rm");
+      leftPully = hardwareMap.get(DcMotor.class, "lp");
+      rightPully = hardwareMap.get(DcMotor.class, "rp");
+      horizontalSlide = hardwareMap.get(DcMotor.class, "hs");
+      colorSensor = hardwareMap.get(ColorSensor.class, "cs");
+      gyroSensor = hardwareMap.get(GyroSensor.class, "gs");
+      middleMotor = hardwareMap.get(DcMotor.class, "mm");
+      colorServo = hardwareMap.get(Servo.class, "colorservo");
+      //lights = hardwareMap.get(DcMotor.class, "lights");
+      relicGrabber = hardwareMap.get(Servo.class, "relicgrabber");
+      relicWrist = hardwareMap.get(Servo.class, "relicwrist");
    }
 
    public void initHardware() {
-       //called during init() of OpMode
-       backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-       backRightMotor.setDirection(DcMotor.Direction.REVERSE);
-       middleMotor.setDirection(DcMotor.Direction.REVERSE);
-       leftMec.setDirection(DcMotor.Direction.FORWARD);
-       rightMec.setDirection(DcMotor.Direction.REVERSE);
-       leftPully.setDirection(DcMotor.Direction.FORWARD);
-       rightPully.setDirection(DcMotor.Direction.REVERSE);
-       //lights.setPower(1.0);
-       horizontalSlide.setDirection(DcMotor.Direction.FORWARD);
-       colorServo.setPosition(SERVO_UP);
-       relicWrist.setPosition(RELIC_OVER);
+      //called during init() of OpMode
+      backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+      backRightMotor.setDirection(DcMotor.Direction.REVERSE);
+      middleMotor.setDirection(DcMotor.Direction.REVERSE);
+      leftMec.setDirection(DcMotor.Direction.FORWARD);
+      rightMec.setDirection(DcMotor.Direction.REVERSE);
+      leftPully.setDirection(DcMotor.Direction.FORWARD);
+      rightPully.setDirection(DcMotor.Direction.REVERSE);
+      //lights.setPower(1.0);
+      horizontalSlide.setDirection(DcMotor.Direction.FORWARD);
+      colorServo.setPosition(SERVO_UP);
+      relicWrist.setPosition(RELIC_OVER);
    }
 }
